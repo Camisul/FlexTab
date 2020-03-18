c31c8a4f3e5825a6a08fef8a4dd2e7daa3460a76
Camisul <7963180+Camisul@users.noreply.github.com>
2020-03-19 00:24:05 +0300

    Tidy up...

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Simple new tab chrome extention written in plain javascript   
 
 Geolocation access is only required for [openweathermap](https://openweathermap.org/api) forecast
-## For forecast you need your own openweathermap api key (look into index.js file)
+## For forecast you need your own openweathermap api key
--- src/index.js
+++ src/index.js
@@ -1,6 +1,88 @@
 //OpenWeatheMap api url & key
-API_KEY = '84d89182eabe041d88c2a0cd0ad03ff6'; //api key goes here
-API_URL = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = '84d89182eabe041d88c2a0cd0ad03ff6';
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
+
+const sec = 1000;
+const min = 60 * sec;
+
+function $(selector){
+  return document.querySelector(selector);
+}
+/**
+ * Get time in format HH:MM and 3 letter day name + DD
+ * @returns {Array<string>} [0] -> time [1] -> Day of the week 
+ */
+function dt(){
+  const d = new Date().toString().split(' ');
+  const sDayOfTheWeek = `${d[0]} ${d[2]}`.toUpperCase();
+  const time = d[4].split(':').slice(0,2).join(':');
+  return [time, sDayOfTheWeek];
+}
+
+function drawTime(){
+  const [time, dayString] = dt();
+  $('#clock').innerText = time;
+  $('#date').innerText = dayString;
+}
+
+function timeLoop() {
+  // Look @: https://stackoverflow.com/a/19089196 
+  const delay = min - (new Date() % min);
+  setTimeout(() => setInterval(drawTime, min), delay);
+}
+
+/**
+ * Convert object string to query string
+ * @returns {string} query string 
+ * @param {*} obj 
+ */
+function obj2QS(obj){
+  const queryBody = Object.entries(obj)
+  // Urlencoding key and value
+  .map(x => x.map(xx => encodeURIComponent(xx)))
+  .map(([k,v]) => `${k}=${v}`)
+  .join('&');
+  return '?' + queryBody;
+}
+/**
+ * @returns {Promise} GeolocationPosition 
+ */
+const getCurrentPosition = () => new Promise((resolve, reject) => {
+  navigator.geolocation.getCurrentPosition(resolve);
+});
+
+function load() {
+  drawTime();  
+
+  getCurrentPosition().then(pos => {
+    const {coords: {latitude: lat, longitude: lon}} = pos;
+  
+    const options = obj2QS({
+      lat,
+      lon,
+      units: 'metric',
+      appid: API_KEY,
+    });
+    
+    fetch(API_URL + options)
+    .then(res => res.json())
+    .then(data => {
+      const {main: {temp}, weather: [{main}]} = data;
+      $('#forecast').innerText = `${temp}° ${main}`;
+    });
+  });
+  timeLoop();
+}
+
+window.onload = load;
+
+/* 
+
+            *****************
+            *               *
+            *      OLD      *
+            *               *
+            *****************
 
 // Date and time related stuff
 
@@ -25,6 +107,7 @@ function getTime() {
   HoursMinsSecs.pop();
   return time = HoursMinsSecs.join(':');
 }
+
 
 window.onload = function () {
   //Geo and forecast related stuff...
@@ -83,4 +166,4 @@ window.onload = function () {
   delay = 60000 - (now % 60000);
   setTimeout(dd, delay);
 };
-
+*/
